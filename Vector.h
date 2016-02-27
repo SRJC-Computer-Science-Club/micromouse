71b97d466f970de0ec8bfc292c4ccc29f00700fe
whothat <benhuff322@gmail.com>
2016-02-26 19:18:41 -0800

    Add commenting to Vector.h

--- Vector.h
+++ Vector.h
@@ -19,27 +19,33 @@ namespace Micromouse
 
 	namespace Vector
 	{
+		// a position vector that represents a coordinate within the maze
+		// it contains x and y integers
+		// the values for x and y are bounded to 0<=x,y<MAZE_W,MAZE_H
 		class Pos
 		{
 		public:
 			Pos( int x , int y );
 			~Pos();
 
-			int x();
-			int y();
-			void x( int x );
-			void y( int y );
+			int x(); // return x
+			int y(); // return y 
+			void x( int x ); // set x, 0<=x<MAZE_W
+			void y( int y ); // set y, 0<=y<MAZE_H
 
 
-			//static const Vector::Pos& UNDEFINED;//a Vector that represents an undefined Vector
+			// static const Vector::Pos& UNDEFINED;//a Vector that represents an undefined Vector
 
 		private:
-			void validateSelf();
+			void validateSelf(); //ensures x and y are within bounds
 
 			int _x , _y;
 		};
 
-
+		// a direction vector that represents a direction and magnitude
+		// it contains a direction 'dir' and int 'mag'
+		// 'dir' cant equal NONE
+		// 'mag' cant be negative or equal to the size of the maze
 
 		class Dir
 		{
@@ -47,13 +53,13 @@ namespace Micromouse
 			Dir( direction dir , int mag );
 			~Dir();
 
-			direction dir();
-			int mag();
-			void dir( direction dir );
-			void mag( int mag );
+			direction dir(); //return the direction
+			int mag(); // return the magnitude
+			void dir( direction dir ); // set the direction, dir != NOONE
+			void mag( int mag ); // set the magnitude, 0 <= mag < maze size
 
 		private:
-			void validateSelf();
+			void validateSelf(); // ensures dir and mag are with bounds
 
 			direction _dir;
 			int _mag;

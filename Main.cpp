69aa58e8a9989ba4b858f41445a228784c0baae2
whothat <benhuff322@gmail.com>
2016-03-05 19:59:12 -0800

    add log statements to Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,10 @@
+#include "Logger.h"
+const LogLevel LOG_LEVEL = LOG_ALL;
+
 #include <iostream>
 #include <cstdlib>
-#include<time.h>
+#include <ctime> //time
+#include <iomanip> //put_time
 #include "Controller.h"
 
 using namespace std;
@@ -8,9 +12,26 @@ using namespace std;
 
 int main( int argc , char** argv )
 {
+	//gets current time and date
+	//thx stackoverflow
+	auto t = std::time( nullptr );
+	auto tm = *std::localtime( &t );
+
+	//log everytime the program runs
+	log( INFO ) << "Program Start: " << std::put_time( &tm , "%c" );
+
+
+	//seed the random function
 	srand( (unsigned int)time( NULL ) );
 
-	Micromouse::Controller routine;//begins the pathfinding routine
+
+	Micromouse::Controller routine;//begins the maze solving routine
+
+
+
+
+
+	log( INFO ) << "Program End \n\n\n";
 
 	return 0;
 }

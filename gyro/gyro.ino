12f1a6976b04c74b9fb81383a19164547d58f7a0
Pastyguy <bibblefirman@gmail.com>
2016-03-18 19:57:48 -0700

    Made code that reads the gyro

---
+++ gyro/gyro.ino
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LSM9DS0.h>
+#include <Adafruit_Sensor.h>  // not used in this demo but required!
+
+// i2c
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
+int rotation = 0;
+int maxValue = 0;
+// You can also use software SPI
+//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(13, 12, 11, 10, 9);
+// Or hardware SPI! In this case, only CS pins are passed in
+//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(10, 9);
+
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+  
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
+}
+
+void setup() 
+{
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  Serial.begin(9600);
+  Serial.println("LSM raw read demo");
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS0. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS0 9DOF");
+  Serial.println("");
+  Serial.println("");
+}
+
+void loop() 
+{
+  for(int count = 0; count <= 1000; count ++){
+    lsm.read();
+    rotation = (int)lsm.gyroData.z;
+    
+    if(maxValue < rotation)
+    {
+      maxValue = rotation;//larges sensor value
+    }
+    delay(1);
+  }
+  Serial.print("Z: "); Serial.print((int)lsm.gyroData.z); 
+  Serial.print(" Max Value: ");   Serial.println(maxValue);
+  //delay(200);//default 200
+}

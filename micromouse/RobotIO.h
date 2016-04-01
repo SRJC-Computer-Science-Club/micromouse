3f2d0de9964f1c591a8a9a0cedd06901cbf64fa5
joshuasrjc <joshuasrjc@gmail.com>
2016-04-01 11:41:32 -0700

    Added pin numbers.

--- micromouse/RobotIO.h
+++ micromouse/RobotIO.h
@@ -5,17 +5,30 @@
 
 namespace Micromouse
 {
-	//TODO use actual pin numbers
-#ifdef __MK20DX256__ //this is the Teensy signature
+	const int IR_FRONT_LEFT_PIN = 14;
+	const int IR_FRONT_RIGHT_PIN = 15;
+	const int IR_LEFT_PIN = 22;
+	const int IR_RIGHT_PIN = 23;
 
-	//comment line below to test compile, remove #error when pins have been set
-	//#error define correct pin numbers 
-#endif
-    const int IR_LEFT_PIN = 1;
-    const int IR_RIGHT_PIN = 2;
-    const int IR_FRONT_LEFT_PIN = 3;
-    const int IR_FRONT_RIGHT_PIN = 4;
-    const int BUTTON_PIN = 10;
+	const int MOTOR_RIGHT_FWD_PIN = 11;
+	const int MOTOR_RIGHT_BACK_PIN = 12;
+	const int MOTOR_RIGHT_PWM_PIN = 9;
+
+	const int MOTOR_LEFT_FWD_PIN = 16;
+	const int MOTOR_LEFT_BACK_PIN = 17;
+	const int MOTOR_LEFT_PWM_PIN = 10;
+
+	const int ENCODER_LEFT_FWD_PIN = 20;
+	const int ENCODER_LEFT_BACK_PIN = 21;
+	const int ENCODER_RIGHT_FWD_PIN = 8;
+	const int ENCODER_RIGHT_BACK_PIN = 7;
+
+	const int BUTTON_PIN = 6;
+	const int switch_A_PIN = 0;
+	const int switch_B_PIN = 2;
+	const int switch_C_PIN = 4;
+
+	const int LED_PIN = 13;
 
 
 	//A class that contains functions and constants used to communicate with the IO pins of the Teensey 3.2 Microcontroller.

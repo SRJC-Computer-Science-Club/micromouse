66a4f07afce6f564813f77c6f20bb9dd09ab0fd1
joshuasrjc <joshuasrjc@gmail.com>
2016-04-01 11:20:09 -0700

    Created Motor Class
    I created an empty Motor class that does nothing.

---
+++ Motor.cpp
@@ -0,0 +1,13 @@
+#include "Motor.h"
+
+#ifdef __MK20DX256__
+
+namespace Micromouse
+{
+	Motor::Motor()
+	{
+
+	}
+}
+
+#endif
---
+++ Motor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifdef __MK20DX256__
+// If compiled for Teensy
+#include "Encoder.h"
+
+namespace Micromouse
+{
+
+	//Motor is a class that can be used to set the direction and speed of a motor,
+	//and to get information from its encoder.
+	class Motor
+	{
+	public:
+		Motor();
+
+	private:
+		int fwdPin;
+		int backPin;
+		int pwmPin;
+		int encoderPin;
+	};
+
+}
+
+#endif

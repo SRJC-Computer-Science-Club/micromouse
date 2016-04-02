dfc843045f19f40ecc13130776254942573c3291
yyekalo <yekaloberhane@Yekalos-MacBook-Pro.local>
2016-04-01 21:55:46 -0700

    removed the moveForward place holder

--- micromouse/RobotIO.cpp
+++ micromouse/RobotIO.cpp
@@ -32,47 +32,47 @@ namespace Micromouse
 
 	/**** SENSORS ****/
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	bool RobotIO::isClearForward()
 	{
 		return true;
 		return !isWallinDirection(N);
 	}
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	bool RobotIO::isClearRight()
 	{
 		return true;
 		return !isWallinDirection(W);
 	}
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	bool RobotIO::isClearLeft()
 	{
 		return true;
         return !isWallinDirection(E);
     }
 
-    
+
     bool RobotIO::isWallinDirection( direction dir )
     {
         // insure valid data
         assert( dir == W || dir ==  N || dir == E || dir == NW || dir == NE);
-        
+
         int numOpen = 0;
-        
+
         for (int i = 0; i != 6; i++)
         {
             #ifdef __MK20DX256__ //this is the Teensy signature
             // TODO test for better intervol
             delay(1);
             #endif
-            
+
             //TODO fill in distances will measured values
             // distances now are just estimates
             switch( dir )
             {
-                
+
                 case W:
                 {
                     if  ( IRSensors[ LEFT ]->getDistance() < 100 )
@@ -80,9 +80,9 @@ namespace Micromouse
                         numOpen++;
                     }
                     break;
-                
+
                 }
-                
+
                 case E:
                 {
                     if ( IRSensors[ RIGHT ]->getDistance() < 100 )
@@ -90,24 +90,24 @@ namespace Micromouse
                         numOpen++;
                     }
                     break;
-                
+
                 }
-                
+
                 case N:
                 {
                     int dist = IRSensors[ FRONT_LEFT ]->getDistance();
-                
+
                     if ( dist < 120 && abs( dist - IRSensors[ FRONT_RIGHT]->getDistance() ) < 30 )
                     {
                         numOpen++;
                     }
                     break;
                 }
-                
+
                 case NW:
                 {
                     int dist = IRSensors[ FRONT_LEFT ]->getDistance();
-                    
+
                     if ( dist < 150 && dist > 110 )
                     {
                         numOpen++;
@@ -117,7 +117,7 @@ namespace Micromouse
                 case NE:
                 {
                     int dist = IRSensors[ FRONT_RIGHT ]->getDistance();
-                    
+
                     if ( dist < 150 && dist > 110 )
                     {
                         numOpen++;
@@ -130,12 +130,12 @@ namespace Micromouse
 
             }
         }
-        
+
         // TODO perfect the amount of tests and amount needed to give true.
         // if only 2 or less of the 6 wall tests came out true
-		return numOpen > 2;    
+		return numOpen > 2;
     }
-    
+
 
 
 
@@ -143,30 +143,26 @@ namespace Micromouse
 
 	/**** MOTORS ****/
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	void RobotIO::moveForward()
 	{
-#ifdef __MK20DX256__
-		digitalWrite(LED_PIN, HIGH);
-		leftMotor.move(1433, 0.5f, 2);
-		digitalWrite(LED_PIN, LOW);
-#endif
+
 
 	}
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	void RobotIO::rotateLeft()
 	{
 
 	}
 
-	// ## NOT YET IMPLEMENTED ## 
+	// ## NOT YET IMPLEMENTED ##
 	void RobotIO::rotateRight()
 	{
 
 	}
 
-    
+
 
 
 

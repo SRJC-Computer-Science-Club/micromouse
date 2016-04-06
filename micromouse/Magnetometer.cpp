78fcbe585c9a5289bec8af72fbccb5e5396cd99d
Pastyguy <bibblefirman@gmail.com>
2016-04-06 01:11:33 -0700

    added comments

--- micromouse/Magnetometer.cpp
+++ micromouse/Magnetometer.cpp
@@ -1,13 +1,18 @@
 #include "Magnetometer.h"
 #include "Logger.h"
 
+
+
 namespace Micromouse
 {
-
 	float Magnetometer::getDegrees()
 	{
 		return getDegreesRaw() - initialDirection;
 	}
+
+
+
+
 
 
 	void Magnetometer::initDirection()
@@ -15,62 +20,84 @@ namespace Micromouse
 		initialDirection = getDegreesRaw();
 	}
 
+
+
+
+
+
+	
 	float Magnetometer::getDegreesRaw()
 	{
-#ifdef __MK20DX256__ 
-		sensorREAD.read();
-		float degrees = atan2(sensorREAD.magData.y, sensorREAD.magData.x);
-		degrees *= 57.296f;
-		return degrees;
-#endif
-		return 0.0f;
+
+	#ifdef __MK20DX256__ 
+
+			sensorREAD.read();
+			float degrees = atan2(sensorREAD.magData.y, sensorREAD.magData.x);
+			degrees *= 57.296f;
+			return degrees;
+
+	#endif
+
+			return 0.0f;
 	}
+
+
+
+
+
 
 
 	direction Magnetometer::getDirection()
 	{
 		return (direction)((int)(getDegrees() - 22.5) / 45);
 	}
-	
-
-
-	
 
 
 
 
 
-	
+
 	void Magnetometer::initSensor()
 	{
 		initDirection();
-#ifdef __MK20DX256__ 
 
+	#ifdef __MK20DX256__ 
 
-		//the magnetometer sensitivity
+		//Set magnetometer sensitivity
 		sensorREAD.setupMag(sensorREAD.LSM9DS0_MAGGAIN_12GAUSS);
 
 		//checks to see if there is data comming from magnetometer
 		if (!sensorREAD.begin())
 		{  
+
 			log(ERROR) << "Oops ... unable to initialize the LSM9DS0. Check your wiring!";
+
 			while (true);
+
 		}
 
-#endif
+	#endif
+
 	}
+
+
 
 
 
 
 	Magnetometer::Magnetometer(const int MAGNETOMETER_NINE_DOF_SDA_PIN, const int MAGNETOMETER_NINE_DOF_SCL_PIN)
 	{
-#ifdef __MK20DX256__ 
+
+	#ifdef __MK20DX256__ 
 
 		initSensor();
 
-#endif
+	#endif
+
 	}
+
+
+
 
 
 
--- micromouse/Magnetometer.h
+++ micromouse/Magnetometer.h
@@ -1,37 +1,60 @@
 #pragma once
 #include "Vector.h"
+
 #ifdef __MK20DX256__
+
 #include <math.h>
 #include "Adafruit_LSM9DS0.h"
+
 #endif
+
+
+
 namespace Micromouse
 {
+
+	/*How to use Magnetometer objects
+	first include the magnetometer header file
+		#include "Magnetometer.h"
+	Then to delare an object use the syntax
+		Magnetometer  NAME = Magnetometer(18,19);
+	or
+		Magnetometer  NAME = Magnetometer(MAGNETOMETER_NINE_DOF_SDA_PIN, MAGNETOMETER_NINE_DOF_SCL_PIN);
+	To call get direction 
+		NAME.getDegrees();
+	or to get the direction the robot it facing 
+	Name.getDirection();
+
+
+	NOTES
+		getDegrees is 0.00 -> 360.00 floats
+		getDirection returns N (North), S (South).... "direction" data type enum of int's
+	*/
+
 	class Magnetometer
 	{
 
 	public:
-		//how to define magnetometer object
-		//#include "Magnetometer.h"
-		//Magnetometer OBJECTNAME = Magnetometer(18,19);
-		//OBJECTNAME.getDegrees();
 
 		float getDegrees();
 		void initDirection();
-		//TODO function to get direction 
 		direction getDirection();
-		
+
 		//initialised the sensor to read data when object is created
 		Magnetometer(const int MAGNETOMETER_NINE_DOF_SDA_PIN, const int MAGNETOMETER_NINE_DOF_SCL_PIN);
-
 		~Magnetometer();
+
 	private:
-		float initialDirection = 0.0f;
-		void initSensor();
+
+		float initialDirection = 0.00f;
+		void  initSensor();
 		float getDegreesRaw();
 
-#ifdef __MK20DX256__ 
+	#ifdef __MK20DX256__ 
+
 		Adafruit_LSM9DS0 sensorREAD = Adafruit_LSM9DS0();
-#endif
+
+	#endif
+
 	};
-	
 }

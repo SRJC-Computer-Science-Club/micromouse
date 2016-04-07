ada24bc44d4e8e0cb7b192b26309acf11432cd49
joshuasrjc <joshuasrjc@gmail.com>
2016-04-06 20:09:27 -0700

    Swapped for PIDController
    I removed the old class PIDControl, and added the new class
    PIDController.

---
+++ micromouse/PIDController.cpp
@@ -0,0 +1,80 @@
+/*********************************\
+File:			PIDController.cpp
+Creation date:	4/5/2016
+
+Author Names:	Joshua Murphy
+				Yyekalo Aberha
+
+Author GitHub:	joshuasrjc
+				yyekalo
+\*********************************/
+
+#include "PIDController.h"
+#include <assert.h>
+#include "Logger.h"
+#ifndef __MK20DX256__
+#include <iostream>
+#endif
+
+
+
+namespace Micromouse
+{
+	void PIDController::start(float initialError)
+	{
+		totalError = 0;
+		lastError = initialError;
+		lastTime = micros();
+
+		started = true;
+	}
+
+	float PIDController::getCorrection(float currentError)
+	{
+		assert(started);
+
+		float deltaTime = getDeltaTime();
+
+		totalError += currentError * deltaTime;
+
+		float pCorrection = P * currentError;
+		float iCorrection = I * totalError;
+		float dCorrention = D * (currentError - lastError) / deltaTime / 10000.0f;
+
+		float sum = pCorrection + iCorrection + dCorrention;
+		sum /= 1000.0f;
+
+		//Total is bound between -1 and 1
+		sum = sum < -1 ? -1 : sum;
+		sum = sum > 1 ? 1 : sum;
+
+		return sum;
+	}
+
+	void PIDController::setConstants(float P, float I, float D)
+	{
+		this->P = P;
+		this->I = I;
+		this->D = D;
+	}
+
+	float PIDController::getDeltaTime()
+	{
+		long currentTime = micros();
+		float deltaTime = (currentTime - lastTime) / 1000000.0f; //1,000,000 microseconds in a second.
+		lastTime = currentTime;
+		return deltaTime;
+	}
+
+#ifndef __MK20DX256__
+	long PIDController::micros()
+	{
+		using namespace std::chrono;
+		
+		return duration_cast<microseconds>(high_resolution_clock::now() - initalTime).count();
+	}
+#endif
+
+}
+
+
---
+++ micromouse/PIDController.h
@@ -0,0 +1,59 @@
+/*********************************\
+File:			PIDController.h
+Creation date:	4/5/2016
+
+Author Names:	Joshua Murphy
+Yyekalo Aberha
+
+Author GitHub:	joshuasrjc
+yyekalo
+\*********************************/
+
+#pragma once
+#ifndef __MK20DX256__
+#include <chrono>
+#endif
+
+namespace Micromouse
+{
+	// This class uses PID control to determine the amount of correction needed
+	class PIDController
+	{
+	public:
+		// Sets the P, I, and D constants for the controller.
+		PIDController(float P, float I, float D)
+			: P(P), I(I), D(D) {}
+
+		// Starts the controller with an initial error, and resets the total error.
+		// MUST be called before calling getCorrection().
+		void start(float initialError);
+
+		// Returns the error correction.
+		// start() MUST be called before calling this function.
+		float getCorrection(float currentError);
+
+		// Sets the P, I, and D constants for the controller.
+		void setConstants(float P, float I, float D);
+
+	private:
+		float getDeltaTime();
+
+#ifndef __MK20DX256__
+		// If on PC
+		long micros();
+
+		typedef std::chrono::steady_clock::time_point time_point;
+		time_point initalTime = std::chrono::high_resolution_clock::now();
+#endif
+
+		bool started = false;
+		float totalError = 0;
+		float lastError = 0;
+
+		float P;
+		float I;
+		float D;
+
+		long lastTime;
+	};
+}

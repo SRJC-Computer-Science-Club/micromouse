256e183f4f13e68ba2da2733df0f2dc9707e382b
Erick <esericksanc@gmail.com>
2016-04-20 20:16:43 -0700

    init Xcode

---
+++ micromouse-xcode/micromouse-xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  micromouse-xcode
+//
+//  Created by Erick Sanchez on 4/20/16.
+//  Copyright © 2016 Erick Sanchez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
